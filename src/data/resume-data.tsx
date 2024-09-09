7cf7d5f214bc01af039062f9b861a8a680791f38
perebaj <perebaj@gmail.com>
2024-09-09 10:54:56 -0300

    :sparkles: update

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -24,8 +24,7 @@ export const RESUME_DATA = {
   initials: "JJ",
   location: "Brazil, GMT-3",
   locationLink: "",
-  about:
-    "Doer and thinker. Having fun hacking backend applications.",
+  about: "Doer and thinker. Having fun hacking backend applications.",
   summary:
     "I'm a software developer with 5 years of expertise in building microservices, event-driven pipelines, and REST APIs using Golang and Python. Additionally, I have experience managing various infrastructure components across different cloud providers such as AWS, Azure, and GCP.",
   avatarUrl: "https://avatars.githubusercontent.com/u/39134841?v=4",
@@ -49,12 +48,28 @@ export const RESUME_DATA = {
   education: [
     {
       school: "Federal University of São Carlos",
-      degree: "Bachelor's Degree in Computer Engineer | Focus on AI and Big Data",
+      degree: "Bachelor's Degree in Computer Engineer",
       start: "2018",
       end: "2023",
-    }
+    },
+    {
+      school: "University of São Paulo - USP",
+      degree: "Master's Degree in Computer Engineer",
+      start: "2024",
+      end: "in progress",
+    },
   ],
   work: [
+    {
+      company: "Tino, Brazil",
+      link: "https://www.tino.com.br/",
+      badges: ["Remote"],
+      title: "Software Engineer",
+      logo: ParabolLogo,
+      start: "April/2023",
+      end: "Current",
+      description: "Building golang microservices and event-driven pipelines",
+    },
     {
       company: "Birdie.ai - Palo Alto, CA",
       link: "https://birdie.ai/",
@@ -64,7 +79,7 @@ export const RESUME_DATA = {
       start: "November/2020",
       end: "December/2023",
       description:
-      "Startup development, starting from the angel investment stage and successfully securing $7 million in seed funding.",
+        "Startup development, starting from the angel investment stage and successfully securing $7 million in seed funding.",
     },
     {
       company: "Federal University of São Carlos - Brazil, SP",
@@ -90,15 +105,14 @@ export const RESUME_DATA = {
     "Event Driven Pipelines",
     "Unit, Integration & Load Tests",
     "Grafana/Prometheus",
-    "ELK Stack"
+    "ELK Stack",
   ],
   projects: [
     {
       title: "My Mind",
-      techStack: [
-        "Engineer Documentation",
-      ],
-      description: "Most of my ideas, notes on engineering, articles, podcasts, and other things I come across are located in this Git repository. Over time, I noticed that the engineers who are references in our field have the habit of documenting their trajectories/careers.",
+      techStack: ["Engineer Documentation"],
+      description:
+        "Most of my ideas, notes on engineering, articles, podcasts, and other things I come across are located in this Git repository. Over time, I noticed that the engineers who are references in our field have the habit of documenting their trajectories/careers.",
       logo: ConsultlyLogo,
       link: {
         label: "",
@@ -107,10 +121,9 @@ export const RESUME_DATA = {
     },
     {
       title: "Newsletter",
-      techStack: [
-        "Golang", "Web Scraping", "Goroutines/Channels", "MongoDB"
-      ],
-      description: "A personal project to scrape the blog posts from my favorite Tech Magicians and sent it to my email.",
+      techStack: ["Golang", "Web Scraping", "Goroutines/Channels", "MongoDB"],
+      description:
+        "A personal project to scrape the blog posts from my favorite Tech Magicians and sent it to my email.",
       logo: ConsultlyLogo,
       link: {
         label: "",
@@ -119,12 +132,9 @@ export const RESUME_DATA = {
     },
     {
       title: "Legitima",
-      techStack: [
-        "Golang",
-        "Authentication/Authorization",
-        "OAuth2"
-      ],
-      description: "Legitima is not a framework; rather, it serves as a centralized authentication system that seamlessly integrates various authenticators such as Google, GitHub, and more.",
+      techStack: ["Golang", "Authentication/Authorization", "OAuth2"],
+      description:
+        "Legitima is not a framework; rather, it serves as a centralized authentication system that seamlessly integrates various authenticators such as Google, GitHub, and more.",
       logo: ConsultlyLogo,
       link: {
         label: "",
@@ -141,9 +151,10 @@ export const RESUME_DATA = {
         "PostgresDB/DB Migrations",
         "Structured Logs",
         "OpenAPI Swagger",
-        "react/nextjs"
+        "react/nextjs",
       ],
-      description: "A Single place to share the underground. This app is designed for people who are looking for unique experiences and want to share their favorite parties/events in their cities.",
+      description:
+        "A Single place to share the underground. This app is designed for people who are looking for unique experiences and want to share their favorite parties/events in their cities.",
       logo: ConsultlyLogo,
       link: {
         label: "",
@@ -162,9 +173,10 @@ export const RESUME_DATA = {
         "Unit & Integration Tests",
         "OpenAPI Swagger",
         "Authentication/Authorization",
-        "OAuth2"
+        "OAuth2",
       ],
-      description: "Jamming around orders with API endpoints 🎸. This project is an attempt to gather some basic concepts of software development, from metrics and logs to authentication.",
+      description:
+        "Jamming around orders with API endpoints 🎸. This project is an attempt to gather some basic concepts of software development, from metrics and logs to authentication.",
       logo: ConsultlyLogo,
       link: {
         label: "",
@@ -173,13 +185,9 @@ export const RESUME_DATA = {
     },
     {
       title: "Metrica",
-      techStack: [
-        "Golang",
-        "Concurrency",
-        "Goroutines/Channels",
-        "Load Tests"
-      ],
-      description: "Studies about the most basically race condition problems using golang concurrency stack",
+      techStack: ["Golang", "Concurrency", "Goroutines/Channels", "Load Tests"],
+      description:
+        "Studies about the most basically race condition problems using golang concurrency stack",
       logo: ConsultlyLogo,
       link: {
         label: "",
@@ -188,9 +196,7 @@ export const RESUME_DATA = {
     },
     {
       title: "JJ Template",
-      techStack: [
-        "Golang",
-      ],
+      techStack: ["Golang"],
       description: "Automation of my golang projects using CLI",
       logo: ConsultlyLogo,
       link: {
@@ -200,10 +206,7 @@ export const RESUME_DATA = {
     },
     {
       title: "Secret🤫🤫🤫",
-      techStack: [
-        "Golang",
-        "Cryptography"
-      ],
+      techStack: ["Golang", "Cryptography"],
       description: "Taking care of my secrets",
       logo: ConsultlyLogo,
       link: {
